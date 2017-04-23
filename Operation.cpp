7b8dafd5a5deee1dc0ebfb8bfde732f5948d9692
You Name <email@example.com>
2017-04-23 13:21:40 +0800

    commit

--- Operation.cpp
+++ Operation.cpp
@@ -5,17 +5,27 @@
 #include <map>
 #include <string.h>
 using namespace std;
+
+char str[11],s1[5],ch,sf[5];
+int n,m,e,i=0,j=0,length=strlen(sf),t=0,son,mom,num[5];
+double s;
+ // str :栈 存储数据  s1: 整数算式  sf: 分数算式  ch: 字符
+ // son mom ： 分子分母 num[5] 对应数字
+ // s 输入的值 
 string Input="Input";
 string True="True";
 string Questions="Questions";
 string Right="Right";
 string Wrong="Wrong";
 string Exit="Exit";
+// 统一命令  
 int flagl=0;
 char Lang;
 map<string ,string >Map;
+// 语言表 
 int flag =0 ;
 int amount =0;
+
 class Question {
       public :
              int randNum();
@@ -24,10 +34,12 @@ class Question {
    };
 int  Question::randNum()
              {    
+                 
                   return 1+rand()%9;
               }
 int  Question::randSym()
              {    
+                  
                   return 1+rand()%4;
              }
 char Question::symbol(int i){
@@ -49,7 +61,8 @@ char Question::symbol(int i){
                     }
                   }
 
-int check(char str[])
+int check(char str[])//用于计算生成的运算式的结果
+
 { 
    int  sum,i,j,t,top=0;
    double d;
@@ -161,6 +174,7 @@ int gcd(int m,int n)
 }  
 
 void Fraction(){
+     
   if(flagl==0)
     {
     True +=Lang;
@@ -171,7 +185,6 @@ void Fraction(){
     flagl=1;
     }
     int a,b,c,d,g1,g2,g3;
-    int son,mom,num[5];
     char s[5],ch;
     a=1+rand()%5;
     b=1+rand()%5;
@@ -189,12 +202,14 @@ void Fraction(){
     g3=gcd(son,mom);
     son/=g3;
     mom/=g3;
-    cin>>s;
-    int t=0,i=0,e,length=strlen(s);
-    ch=s[t];
+}
+void FractionPrint()
+{
+    cin>>sf;
+    ch=sf[t];
     for(t=0;t<length;t++)
     {
-     ch=s[t];
+     ch=sf[t];
      switch(ch)
      {
       case 'g': cout <<Map[True]<<amount<<Map[Questions]<<endl;
@@ -207,7 +222,7 @@ void Fraction(){
                while(ch>='0' && ch<='9')   /*判定为数字字符*/
 		         { 
                  e=10*e+ch-'0';  /*将数字字符转换成对应的数值*/
-                 t++;ch=s[t];
+                 t++;ch=sf[t];
                  }
 		        num[i]=e;
                 i++;
@@ -217,9 +232,11 @@ void Fraction(){
                           
     }
     if(num[0]==son&&num[1]==mom) {cout <<Map[Right]<<endl; amount++;}
-    else cout <<Map[Wrong]<<son<<'/'<<mom<<endl; 
+    else cout <<Map[Wrong]<<son<<'/'<<mom<<endl;    
+     
 }
 void Rand(){
+     
    if(flagl==0)
     {
     True +=Lang;
@@ -233,10 +250,7 @@ void Rand(){
     int num[5];
     int sign[5];
     char sym[5]; 
-    char str[11];
-    char s1[5],ch;
     int count;
-    double s;
     Question q;
     for(i=1;i<=4;i++)
       num[i]=q.randNum();
@@ -256,7 +270,10 @@ void Rand(){
     for(i=1;i<=9;i++)
       cout <<str[i];
     cout <<"=";
-    cin >>s1;
+}
+void RandPrint()
+{
+     cin >>s1;
     str[10]='#';
     s=check(str);
 	if (s1[0] == 'g')
@@ -279,25 +296,21 @@ void Rand(){
       cout <<Map[Wrong]<<s<<endl; 
     }
     else 
-     Rand();  
+     Rand();   
 }
-
-
-int main()
+void scan()
 {
- 
- srand((unsigned)time(NULL));
  char L;
- int n,m;
+
  char str[100];
  char str1[100];
-FILE *fp;
-int max=0;
-string s,s1;
-char ch[1000][1024];
-int i=0,j=0; 
-fp=fopen("Language.txt","r");
-ch[0][0]=fgetc(fp); 
+ FILE *fp;
+ int max=0;
+ string s,s1;
+ char ch[1000][1024];
+
+ fp=fopen("Language.txt","r");
+ ch[0][0]=fgetc(fp); 
 while(!feof(fp)) 
 {
  if(ch[i][j]=='\n')
@@ -339,13 +352,26 @@ for(i=5;i<=m;i++)
  Input +=Lang;
  cout <<Map[Input];
  cin>>n;
-for(i=1;i<=n;i++)
+ }
+
+int main()
+{
+ srand((unsigned)time(NULL));
+ scan();//该函数用于与用户交互，获取输入数据 
+ for(i=1;i<=n;i++)
            {
             if(i%5==0)
-            Fraction();
+            {
+            Fraction(); //用于随机生成分数算式 
+            FractionPrint();//将统计结果输出给用户
+            }
             else
-            Rand();                                           
+            {
+            Rand();  //用于随机生成整数算式    
+            RandPrint();//将统计结果输出给用户
+            }                                      
            } 
-
+ // 随机生成数字跟符号打包成一个函数了  计算判断也在随机生成算式函数内部里          
+   return 0; 
 }
 
